09d5830b256533940ae8f8f8446d8bba6365f0cb
crusat <crusat@crusat.ru>
2013-10-20 11:53:14 +0400

    isLeapYear() not complete

--- demo.js
+++ demo.js
@@ -1,5 +1,19 @@
 function ready() {
+    // current time
     document.getElementById('currentTime').innerHTML = tempus.now();
+    // leap years
+    var domYears = document.getElementById('years');
+    for (var year = 1900; year < 2013; year++) {
+        if (year % 10 === 0) {
+            if (tr !== undefined) {
+                domYears.appendChild(tr);
+            }
+            var tr = document.createElement('tr');
+        }
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(year + (tempus.isLeapYear(year) ? ' Leap': '')));
+        tr.appendChild(td);
+    }
 }
 
 window.addEventListener("load", ready, false);
--- tempus.js
+++ tempus.js
@@ -2,6 +2,14 @@ var TempusJS = function() {
     this.now = function() {
         return Math.floor((new Date()).getTime()/1000);
     };
+    this.isLeapYear = function(year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else return true;
+        }
+        return false;
+    }
 };
 
 var tempus = new TempusJS();
