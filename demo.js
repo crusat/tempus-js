38d886887088966fbbd09fa88255b42591a14848
crusat <crusat@crusat.ru>
2013-10-20 12:25:44 +0400

    daysInMonth

--- demo.js
+++ demo.js
@@ -1,4 +1,5 @@
 function ready() {
+    var tr, td;
     // current time
     document.getElementById('currentTime').innerHTML = tempus.now();
     // leap years
@@ -6,15 +7,25 @@ function ready() {
     var current_year = 2013;
     for (var year = 1900; year <= current_year; year++) {
         if (year % 20 === 0) {
-            var tr = document.createElement('tr');
+            tr = document.createElement('tr');
         }
-        var td = document.createElement('td');
+        td = document.createElement('td');
         td.appendChild(document.createTextNode(year + (tempus.isLeapYear(year) ? ' Leap': '')));
         tr.appendChild(td);
         if ((year % 10 === 0)||(year === current_year)) {
             domYears.appendChild(tr);
         }
     }
+    // days in month
+    var MONTH_COUNT = 12;
+    var domDaysInMonth = document.getElementById('daysInMonth');
+    tr = document.createElement('tr');
+    for (var month = 1; month <= MONTH_COUNT; month++) {
+        td = document.createElement('td');
+        td.appendChild(document.createTextNode(tempus.getDaysCountInMonth(month)));
+        tr.appendChild(td);
+    }
+    domDaysInMonth.appendChild(tr);
 }
 
 window.addEventListener("load", ready, false);
