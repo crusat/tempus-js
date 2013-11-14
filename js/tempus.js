0d98f21d442b2c6e5f9765a4ee3fbf45bac023ad
crusat <crusat@crusat.ru>
2013-11-14 14:21:54 +0400

    v0.2.0 dev

--- js/tempus.js
+++ js/tempus.js
@@ -765,6 +765,17 @@
                     this.seconds() !== undefined ? this.seconds() : 0,
                     this.milliseconds() !== undefined ? this.milliseconds() : 0
                 );
+            },
+            asVanillaDateUTC: function() {
+                return new Date(Date.UTC(
+                    this.year() !== undefined ? this.year() : 1970,
+                    this.month() !== undefined ? this.month() - (monthFromZero ? 0 : 1) : 0,
+                    this.day() !== undefined ? this.day() : 1,
+                    this.hours() !== undefined ? this.hours() : 0,
+                    this.minutes() !== undefined ? this.minutes() : 0,
+                    this.seconds() !== undefined ? this.seconds() : 0,
+                    this.milliseconds() !== undefined ? this.milliseconds() : 0
+                ));
             }
 
 
